b5851d25f5773a177b8043d46d2fc833c1497c54
zhang-ziyang <3574637541@qq.com>
2026-09-15 03:24:14 +0000

    finish pa1 6 wp

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -9,7 +9,13 @@ typedef struct watchpoint {
 
 	/* TODO: Add more members if necessary */
 
+	char expression[128];
+	uint32_t old_value;
 
 } WP;
 
 #endif
+
+WP *new_wp(void);
+void free_wp(WP *wp);
+WP *get_wp_head(void);
--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -20,4 +20,55 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+WP *new_wp(void) {//分配监视点
+	WP *wp;
 
+	if (free_ == NULL) {
+		printf("No free watchpoint\n");
+		return NULL;
+	}
+
+	/* 从空闲链表取下第一个节点。 */
+	wp = free_;
+	free_ = free_->next;
+
+	/* 把它放到使用中链表的开头。 */
+	wp->next = head;
+	head = wp;
+
+	return wp;
+}
+
+void free_wp(WP *wp) {//释放监视点
+	WP *prev;
+
+	if (wp == NULL) {
+		return;
+	}
+
+	/* wp 正好是使用中链表的第一个节点。 */
+	if (head == wp) {
+		head = wp->next;
+	} else {
+		prev = head;
+
+		while (prev != NULL && prev->next != wp) {
+			prev = prev->next;
+		}
+
+		if (prev == NULL) {
+			printf("Watchpoint is not active\n");
+			return;
+		}
+
+		prev->next = wp->next;
+	}
+
+	/* 放回空闲链表开头。 */
+	wp->next = free_;
+	free_ = wp;
+}
+
+WP *get_wp_head(void) {
+	return head;
+}
