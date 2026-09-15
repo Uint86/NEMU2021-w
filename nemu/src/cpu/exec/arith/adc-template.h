e97d3ddfa59254a3561c3bc29abeb7a2a93f78ae
zhang-ziyang <3574637541@qq.com>
2026-09-15 13:30:42 +0000

    40

--- nemu/src/cpu/exec/arith/adc-template.h
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -2,14 +2,37 @@
 
 #define instr adc
 
-static void do_execute () {
-	DATA_TYPE result = op_dest->val + op_src->val + cpu.eflags.CF;
+// static void do_execute () {
+// 	DATA_TYPE result = op_dest->val + op_src->val + cpu.eflags.CF;
+// 	OPERAND_W(op_dest, result);
+
+// 	/* TODO: Update EFLAGS. */
+// 	update_eflags_pf_zf_sf((DATA_TYPE_S)result);
+// 	cpu.eflags.CF = result < op_dest->val;
+// 	cpu.eflags.OF = MSB(~(op_dest->val ^ op_src->val) & (op_dest->val ^ result));
+
+// 	print_asm_template2();
+// }
+
+static void do_execute() {
+	DATA_TYPE dest = op_dest->val;
+	DATA_TYPE src = op_src->val;
+	uint8_t old_cf = cpu.eflags.CF;
+
+	uint64_t wide_result =
+		(uint64_t)dest + (uint64_t)src + old_cf;
+
+	DATA_TYPE result = (DATA_TYPE)wide_result;
+
 	OPERAND_W(op_dest, result);
 
-	/* TODO: Update EFLAGS. */
 	update_eflags_pf_zf_sf((DATA_TYPE_S)result);
-	cpu.eflags.CF = result < op_dest->val;
-	cpu.eflags.OF = MSB(~(op_dest->val ^ op_src->val) & (op_dest->val ^ result));
+
+	cpu.eflags.CF =
+		(wide_result >> (DATA_BYTE * 8)) & 1;
+
+	cpu.eflags.OF =
+		MSB(~(dest ^ src) & (dest ^ result));
 
 	print_asm_template2();
 }
--- nemu/src/cpu/exec/arith/sbb-template.h
+++ nemu/src/cpu/exec/arith/sbb-template.h
@@ -2,14 +2,36 @@
 
 #define instr sbb
 
-static void do_execute () {
-	DATA_TYPE result = op_dest->val - (op_src->val + cpu.eflags.CF);
+// static void do_execute () {
+// 	DATA_TYPE result = op_dest->val - (op_src->val + cpu.eflags.CF);
+// 	OPERAND_W(op_dest, result);
+
+// 	/* TODO: Update EFLAGS. */
+// 	update_eflags_pf_zf_sf((DATA_TYPE_S)result);
+// 	cpu.eflags.CF = result < op_dest->val;
+// 	cpu.eflags.OF = MSB(~(op_dest->val ^ op_src->val) & (op_dest->val ^ result));
+
+// 	print_asm_template2();
+// }
+
+static void do_execute() {
+	DATA_TYPE dest = op_dest->val;
+	DATA_TYPE src = op_src->val;
+	uint8_t old_cf = cpu.eflags.CF;
+
+	uint64_t subtrahend = (uint64_t)src + old_cf;
+	DATA_TYPE result =
+		(DATA_TYPE)((uint64_t)dest - subtrahend);
+
 	OPERAND_W(op_dest, result);
 
-	/* TODO: Update EFLAGS. */
 	update_eflags_pf_zf_sf((DATA_TYPE_S)result);
-	cpu.eflags.CF = result < op_dest->val;
-	cpu.eflags.OF = MSB(~(op_dest->val ^ op_src->val) & (op_dest->val ^ result));
+
+	/* A borrow occurs when dest is smaller than src + old CF. */
+	cpu.eflags.CF = (uint64_t)dest < subtrahend;
+
+	cpu.eflags.OF =
+		MSB((dest ^ src) & (dest ^ result));
 
 	print_asm_template2();
 }
