16c3d237759ca27506e087a673ee6b6f6d23d2e8
zhang-ziyang <3574637541@qq.com>
2026-09-15 11:23:07 +0000

    17

--- nemu/src/cpu/exec/control/jcc.c
+++ nemu/src/cpu/exec/control/jcc.c
@@ -47,3 +47,15 @@ make_helper(jne_si_b) {
 
 	return len + 1;
 }
+
+make_helper(jge_si_b) {
+	int len = decode_si_b(eip + 1);
+
+	if (cpu.eflags.SF == cpu.eflags.OF) {
+		cpu.eip += op_src->simm;
+	}
+
+	print_asm("jge %x", eip + len + 1 + op_src->simm);
+
+	return len + 1;
+}
--- nemu/src/cpu/exec/control/jcc.h
+++ nemu/src/cpu/exec/control/jcc.h
@@ -5,5 +5,6 @@ make_helper(je_si_b);
 make_helper(jbe_si_b);
 make_helper(jle_si_b);
 make_helper(jne_si_b);
+make_helper(jge_si_b);
 
 #endif
