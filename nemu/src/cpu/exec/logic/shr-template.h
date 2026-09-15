39c9858c3c6716274b3531f6c830bfe63e587337
zhang-ziyang <3574637541@qq.com>
2026-09-15 12:28:54 +0000

    32

--- nemu/src/cpu/exec/logic/shr-template.h
+++ nemu/src/cpu/exec/logic/shr-template.h
@@ -2,14 +2,24 @@
 
 #define instr shr
 
-static void do_execute () {
-	DATA_TYPE src = op_src->val;
+static void do_execute() {
 	DATA_TYPE dest = op_dest->val;
+	uint8_t count = op_src->val & 0x1f;
 
-	uint8_t count = src & 0x1f;
-	dest >>= count;
-	OPERAND_W(op_dest, dest);
-        update_eflags_pf_zf_sf(dest);
+	if (count != 0) {
+		DATA_TYPE result = dest >> count;
+
+		/* The last bit shifted out enters CF. */
+		cpu.eflags.CF = (dest >> (count - 1)) & 0x1;
+
+		/* For SHR by one bit, OF receives the original MSB. */
+		if (count == 1) {
+			cpu.eflags.OF = MSB(dest);
+		}
+
+		OPERAND_W(op_dest, result);
+		update_eflags_pf_zf_sf((DATA_TYPE_S)result);
+	}
 
 	print_asm_template2();
 }
