d750eb57bcb0fc012beb23919800c566831a92f4
zhang-ziyang <3574637541@qq.com>
2026-09-15 02:18:40 +0000

    finish switch

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -7,21 +7,21 @@
 #include <regex.h>
 
 enum {
-	NOTYPE = 256, EQ
+	NOTYPE = 256, EQ,
 
 	/* TODO: Add more token types */
-
+	TK_NUM
 };
 
 static struct rule {
-	char *regex;
+	char *regex;//regular expression 正则表达式
 	int token_type;
 } rules[] = {
 
 	/* TODO: Add more rules.
 	 * Pay attention to the precedence level of different rules.
 	 */
-
+	{"[0-9]+",TK_NUM},//number
 	{" +",	NOTYPE},				// spaces
 	{"\\+", '+'},					// plus
 	{"==", EQ}						// equal
@@ -57,16 +57,17 @@ Token tokens[32];
 int nr_token;
 
 static bool make_token(char *e) {
-	int position = 0;
-	int i;
-	regmatch_t pmatch;
+	int position = 0;//位置
+	int i;//正则rule对应序号
+	regmatch_t pmatch;//正则表达式匹配结果
 	
-	nr_token = 0;
+	nr_token = 0;//数量
 
-	while(e[position] != '\0') {
+	while(e[position] != '\0') {//读到结尾
 		/* Try all rules one by one. */
 		for(i = 0; i < NR_REGEX; i ++) {
 			if(regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+				//规则匹配成功  且  匹配内容从当前位置开始
 				char *substr_start = e + position;
 				int substr_len = pmatch.rm_eo;
 
@@ -77,9 +78,28 @@ static bool make_token(char *e) {
 				 * to record the token in the array `tokens'. For certain types
 				 * of tokens, some extra actions should be performed.
 				 */
+				switch (rules[i].token_type) {
+					case NOTYPE:
+						break;
 
-				switch(rules[i].token_type) {
-					default: panic("please implement me");
+					case TK_NUM:
+						Assert(nr_token < 32, "too many tokens");
+						Assert(substr_len < 32, "number is too long");
+
+						tokens[nr_token].type = TK_NUM;
+						strncpy(tokens[nr_token].str,
+								substr_start, substr_len);
+						tokens[nr_token].str[substr_len] = '\0';
+						nr_token++;
+						break;
+
+					default:
+						Assert(nr_token < 32, "too many tokens");
+
+						tokens[nr_token].type = rules[i].token_type;
+						tokens[nr_token].str[0] = '\0';
+						nr_token++;
+						break;
 				}
 
 				break;
